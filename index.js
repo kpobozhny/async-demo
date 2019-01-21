ad9910202356c4a397229e9737902adc506c4923
Kostiantyn Pobozhni <k.pobozhny@gmail.com>
2019-01-21 19:38:55 +0200

    Perform exercise

--- index.js
+++ index.js
@@ -7,11 +7,27 @@ console.log('Before');
 //     });
 // });
 
-getUser(1)
+// Promise-based approach
+/*getUser(1)
     .then(user => getRepositories(user.githubUsername))
     .then(repos => getCommits(repos[0]))
     .then(commits => console.log(commits))
     .catch(err => console.log('Error', err.message));
+*/
+// Async and Await approach
+async function displayCommits(){
+    try{
+        const user = await getUser(1);
+        const repos = await getRepositories(user.githubUsername);
+        const commits = await getCommits(repos[0]);
+        console.log(commits);
+    }
+    catch (err){
+        console.log('Error', err.message);
+    }
+
+}
+displayCommits()
 
 console.log('After');
 
